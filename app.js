7347e688202fa25e4ab66f83145d9e45c340fd4d
weichang0307 <83111522+weichang0307@users.noreply.github.com>
2022-04-16 17:04:08 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 var express = require('express')
 var app = express()
 app.post('/',(req,res)=>{
-    //console.log(1)
+    console.log(1)
     res.send('1')
 })
 app.get('/',(req,res)=>{
-   // console.log(2)
+    console.log(2)
     res.send('2')
 
 })
 app.post('/callback',(req,res)=>{
-   // console.log(3)
+    console.log(3)
     res.send('3')
 
 })
 app.get('/callback',(req,res)=>{
-    //console.log(4)
+    console.log(4)
     res.send('4')
 
 })
