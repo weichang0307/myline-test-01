29854cc30d9217789dfb2d6a0612982052c5d1f2
weichang0307 <83111522+weichang0307@users.noreply.github.com>
2022-04-16 16:59:42 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var app = express()
+app.post('/',(req,res)=>{
+    //console.log(1)
+    res.send('1')
+})
+app.get('/',(req,res)=>{
+   // console.log(2)
+    res.send('2')
+
+})
+app.post('/callback',(req,res)=>{
+   // console.log(3)
+    res.send('3')
+
+})
+app.get('/callback',(req,res)=>{
+    //console.log(4)
+    res.send('4')
+
+})
+
+
+var PORT = process.env.PORT || 3000
+var server = app.listen(PORT,function () {
+
+})
